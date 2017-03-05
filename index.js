7448a76663b98e592a757fa452ab55c77c8aa4f4
David Puerto <me@davidpuerto.com>
2017-03-04 20:04:15 -0800

    starting to scaffold out my front-end and webpack considerations

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
 const express = require('express')
 const app = express()
-
-app.get('/', (req, res) => {
-  res.send('HEY!')
-  console.log('first the req:', req)
-  console.log('then the res:', res)
-})
-
-app.listen(3000, () => console.log('Server running on port 3000'))
