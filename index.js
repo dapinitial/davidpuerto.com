af8b28a4c4a88339b921be51b694e087a6d1f176
David Puerto <me@davidpuerto.com>
2017-03-04 23:15:40 -0800

    switched server to port 3001 and added scss partials

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const express = require('express')
 const app = express()
 
-// app.get('/', (req, res) => {
-//   res.send('HEY!')
-// })
-
-app.listen(3000, () => console.log('Server running on port 3000'))
+app.listen(3001, () => console.log('Server running on port 3001'))
