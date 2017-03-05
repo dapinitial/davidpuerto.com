f64cbd66e6c56473a2ea133dd4d4fa7d13fd58a0
David Puerto <me@davidpuerto.com>
2017-03-04 18:24:10 -0800

    just want to see my server

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
+
 app.get('/', (req, res) => {
   res.send('HEY!')
+  console.log('first the req:', req)
+  console.log('then the res:', res)
 })
+
 app.listen(3000, () => console.log('Server running on port 3000'))
