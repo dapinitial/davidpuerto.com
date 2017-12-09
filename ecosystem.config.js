87beaf5fe2948125b3247f4c16e8ceb4ded8d82b
David Puerto <me@davidpuerto.com>
2017-12-08 17:41:11 -0800

    changing host

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,12 +12,12 @@ module.exports = {
   }],
   deploy: {
     production: {
-      user: 'ec2-user',
-      host: 'ec2-35-163-175-145.us-west-2.compute.amazonaws.com',
+      user: 'ubuntu',
+      host: 'ec2-34-211-70-142.us-west-2.compute.amazonaws.com',
       key: '~/.ssh/dap.pem',
       ref: 'origin/master',
       repo: 'git@github.com:dapinitial/davidpuerto.com.git',
-      path: '/home/ec2-user/davidpuerto.com',
+      path: '/home/ubuntu/davidpuerto.com',
       'post-deploy': 'npm install;npm run build;pm2 stop;killall -9 node;npm run dev;pm2 start index.js --name "davidpuerto.com" ecosystem.config.js'
     }
   }
