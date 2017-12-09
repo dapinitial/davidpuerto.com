92bc94d23897e0725e2906c9afa3569592219e7a
David Puerto <me@davidpuerto.com>
2017-12-09 00:25:38 -0800

    moving to port 80

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const express = require('express')
 const app = express()
 
-app.listen(8081, () => console.log('Server running on port 8081'))
+app.listen(80, () => console.log('Server running on port 80'))
